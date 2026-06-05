fba76cf79b623918bdf567a797d5a56d88bb2afe
Orveo <orveo1902@gmail.com>
2026-06-05 13:43:07 -0400

    Se termino el proyecto

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,35 +1,29 @@
+"use client"
+import useSWR from 'swr'
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
-import { prisma } from "@/src/lib/prisma";
+import { OrderWithProducts } from '@/src/types';
 
-async function getPendingsOrders () {
-  const orders = await prisma.order.findMany(
-    {
-      where: {
-        status: false
-      },
-      include: {
-        orderProducts: {
-          include: {
-            product: true
-          }
-        }
-      }
-    }
-  )
-  return orders
-}
 
-export default async function OrdersPage() {
-  const orders = await getPendingsOrders()
 
-  return (
+export default function OrdersPage() {
+
+  const url = '/admin/orders/api'
+  const fetcher = () => fetch(url).then(res => res.json()).then(data => data)
+  const {data, error, isLoading} = useSWR<OrderWithProducts[]>(url,fetcher,{
+    refreshInterval:1000,
+    revalidateOnFocus: false
+  })
+
+  if(isLoading) <p>Cargando...</p>
+
+  if(data)return (
     <>
-     <Heading>Administrar Ordenes</Heading> 
+     <Heading>Administrar Ordenes</Heading>
 
-     {orders.length ? (
+     {data.length ? (
         <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-          {orders.map(order => (
+          {data.map(order => (
             <OrderCard
               key={order.id}
               order={order}
--- components/products/ImageUpload.tsx
+++ components/products/ImageUpload.tsx
@@ -1,10 +1,12 @@
 "use client"
+import { getImagePath } from "@/src/utils"
 import { CldUploadWidget } from "next-cloudinary"
 import Image from "next/image"
 import { useState } from "react"
 import { TbPhotoPlus } from "react-icons/tb"
 
-export default function ImageUpload() {
+
+export default function  ImageUpload({image}: {image: string | undefined}) {
     const [imageUrl, setImageUrl] = useState('')
 
     return (
@@ -42,10 +44,24 @@ export default function ImageUpload() {
                             )}
                         </div>
                     </div>
+
+                    {image && !imageUrl && (
+                        <div className="space-y-2">
+                            <label>Imagen Actual: </label>
+                            <div className="relative w-64 h-64">
+                                <Image
+                                    fill
+                                    src={getImagePath(image)}
+                                    alt="Image del producto"
+                                />
+                            </div>
+                        </div>
+                    )}
+
                     <input
                         type="hidden"
                         name="image"
-                        value={imageUrl}
+                        value={imageUrl ? imageUrl : image}
                     />
                 </>
             )}
