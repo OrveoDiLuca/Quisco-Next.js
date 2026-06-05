4a33ca5253cca02805b2a54f4c9ceee7a338286f
Orveo <orveo1902@gmail.com>
2026-06-05 10:27:56 -0400

    Se puede agregar un nuevo producto

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,5 @@
 import { Product } from "@/src/generated/prisma/client"
-import { formatCurrency } from "@/src/utils"
+import { formatCurrency, getImagePath } from "@/src/utils"
 import Image from "next/image"
 import AddProduct from "./AddProduct"
 
@@ -9,11 +9,13 @@ type ProductCardProps = {
 }
 
 export default function ProductCard({ product }: ProductCardProps) {
+    const imagePath = getImagePath(product.image)
+
     return (
         <div className="border bg-white">
 
             <Image
-                src={`/products/${product.image}.jpg`}
+                src={imagePath}
                 alt={`Imagen platillo ${product.name}`}
                 width={400}
                 height={500}
